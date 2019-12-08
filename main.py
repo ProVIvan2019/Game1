723c0a8feb6a61f06c2df4bf0ec2310914f98ae5
ProVIvan2019 <ProVIvan2019@yandex.ru>
2019-12-08 13:31:28 +1000

    привет

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 import pygame
 
+level = [
+    '--------------------------',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '-                        -',
+    '--------------------------'
+]
+
 WIN_WIDTH, WIN_HEIGHT = 780, 630
 BG_COLOR = (192, 192, 192)
+BRICK_WIDTH = BRICK_HEIGHT = 30
+BRICK_COLOR = (0, 128, 0)
 
 pygame.init()
 pygame.display.set_caption("первая игра")
 screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+
+brick = pygame.Surface((BRICK_WIDTH, BRICK_HEIGHT))
+brick.fill(BRICK_COLOR)
 
 run = True
 while run:
@@ -14,4 +43,15 @@ while run:
             run = False
 
     screen.fill(BG_COLOR)
+
+    x = 0
+    y = 0
+    for row in level:
+        for col in row:
+            if col == '-':
+                screen.blit(brick, (x, y))
+            x += BRICK_WIDTH
+        y += BRICK_HEIGHT
+        x = 0
+
     pygame.display.update()
