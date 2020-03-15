b3387994876bf1df47026ad397978defdddc8106
ProVIvan2019 <ProVIvan2019@yandex.ru>
2020-03-15 13:28:23 +1000

    d gd v dfbvfbfyuvgrhgghjbfhbnj g

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame
+import sounddevice as sd
+import numpy
+
 
 level = [
     '----------------------------------------------------------------------------------------------------------------------------------',
@@ -26,10 +29,10 @@ BG_COLOR = (192, 192, 192)
 BRICK_WIDTH = BRICK_HEIGHT = 30
 BRICK_COLOR = (0, 128, 0)
 BRICK_COLOR_2 = (255, 128, 0)
-FPS = 60
+FPS = 30
 clock = pygame.time.Clock()
 PLAYER_SIZE = 40
-BG_SPEED = 0.3
+BG_SPEED = 100
 dx = 0
 PLAYER_SPEED = 3
 penalty = 0.0
@@ -64,76 +67,97 @@ btn.fill(BLUE)
 text1 = 'ИГРАТЬ СНОВА ?'
 text1_xy = text.size(text1)
 
+result = [WIN_HEIGHT // 2]
+yyy = [player_rect.y] * 100
+
+
+def audio_callback(indata, frames, time, status):
+    volume = numpy.linalg.norm(indata) * 20
+    # print(volume, end=' ', flush=True)
+    yyy.append(volume)
+    yyy.pop(0)
+    result[0] = sum(yyy) / len(yyy)
+
+
 color = BLUE
 face(BLUE)
 run = True
-while run:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT or e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
-            run = False
-        elif e.type == pygame.MOUSEBUTTONDOWN:
-            if e.button == 1:
-                mouse_pos = pygame.mouse.get_pos()
-                if (
-                    mouse_pos[0] >= (WIN_WIDTH - BTN_W) // 2
-                    and mouse_pos[0] <= (WIN_WIDTH + BTN_W) // 2
-                    and mouse_pos[1] >= WIN_HEIGHT // 2
-                    and mouse_pos[1] <= WIN_HEIGHT // 2 + BTN_H
-                ):
-                    print('!!!!!!!!!!!', end=' ')
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_RIGHT]:
-        player_rect.x += PLAYER_SPEED
-    if keys[pygame.K_LEFT]:
-        player_rect.x -= PLAYER_SPEED
-    if keys[pygame.K_UP]:
-        player_rect.y -= PLAYER_SPEED
-    if keys[pygame.K_DOWN]:
-        player_rect.y += PLAYER_SPEED
-
-    screen.fill(BG_COLOR)
-    if color == RED:
-        color = BLUE
-        face(color)
-
-    if dx > -WIN_WIDTH * 4:
-        dx -= BG_SPEED
-    else:
-        if player_rect.x < WIN_WIDTH - PLAYER_SIZE:
-            player_rect.x += PLAYER_SPEED
-
-    x = dx
-    y = 0
-    for row in level:
-        for col in row:
-            if col == '-':
-                brick = pygame.draw.rect(screen, BRICK_COLOR, [x, y, BRICK_WIDTH, BRICK_HEIGHT])
-                pygame.draw.rect(screen, BRICK_COLOR_2, [x, y, BRICK_WIDTH, BRICK_HEIGHT], 2)
-                if brick.colliderect(player_rect):
-                    if player_rect.x < WIN_WIDTH - PLAYER_SIZE * 2:
-                        if color == BLUE:
-                            color = RED
+stream = sd.InputStream(callback=audio_callback)
+with stream:
+    while run:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT or e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+                run = False
+            elif e.type == pygame.MOUSEBUTTONDOWN:
+                if e.button == 1:
+                    mouse_pos = pygame.mouse.get_pos()
+                    if (
+                        mouse_pos[0] >= (WIN_WIDTH - BTN_W) // 2
+                        and mouse_pos[0] <= (WIN_WIDTH + BTN_W) // 2
+                        and mouse_pos[1] >= WIN_HEIGHT // 2
+                        and mouse_pos[1] <= WIN_HEIGHT // 2 + BTN_H
+                    ):
+                        print('!!!!!!!!!!!', end=' ')
+                        penalty = 0
+                        dx = 0
+                        player_rect.center = WIN_WIDTH // 2, WIN_HEIGHT // 2
+                        yyy = [player_rect.y] * 100
+                        color = BLUE
                         face(color)
-                        penalty += 0.1
-            x += BRICK_WIDTH
-        y += BRICK_HEIGHT
+
+        player_rect.y = WIN_HEIGHT - result[0]
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            player_rect.x += PLAYER_SPEED
+        if keys[pygame.K_LEFT]:
+            player_rect.x -= PLAYER_SPEED
+        if keys[pygame.K_UP]:
+            player_rect.y -= PLAYER_SPEED
+        if keys[pygame.K_DOWN]:
+            player_rect.y += PLAYER_SPEED
+
+        screen.fill(BG_COLOR)
+        if color == RED:
+            color = BLUE
+            face(color)
+
+        if dx > -WIN_WIDTH * 4:
+            dx -= BG_SPEED
+        else:
+            if player_rect.x < WIN_WIDTH - PLAYER_SIZE:
+                player_rect.x += PLAYER_SPEED
+
         x = dx
+        y = 0
+        for row in level:
+            for col in row:
+                if col == '-':
+                    brick = pygame.draw.rect(screen, BRICK_COLOR, [x, y, BRICK_WIDTH, BRICK_HEIGHT])
+                    pygame.draw.rect(screen, BRICK_COLOR_2, [x, y, BRICK_WIDTH, BRICK_HEIGHT], 2)
+                    if brick.colliderect(player_rect):
+                        if player_rect.x < WIN_WIDTH - PLAYER_SIZE * 2:
+                            if color == BLUE:
+                                color = RED
+                            face(color)
+                            penalty += 0.1
+                x += BRICK_WIDTH
+            y += BRICK_HEIGHT
+            x = dx
 
-    if player_rect.x < WIN_WIDTH - PLAYER_SIZE:
-        screen.blit(player, player_rect)
-        screen.blit(
-            text.render(f'Штрафных очков: {round(penalty, 1)}', True, RED, None), text_xу)
-    else:
-        screen.blit(btn, ((WIN_WIDTH - BTN_W) // 2, WIN_HEIGHT // 2))
-        btn.blit(
-            text.render(text1, True, WHITE, None),
-            ((BTN_W - text1_xy[0]) // 2, (BTN_H - text1_xy[1]) // 2))
-        screen.blit(
-            text.render(f'Штрафных очков: {round(penalty, 1)}', True, RED, None),
-            ((WIN_WIDTH - text.size(f'Штрафных очков: {round(penalty, 1)}')[0]) // 2,
-             WIN_HEIGHT // 2 - BTN_H))
+        if player_rect.x < WIN_WIDTH - PLAYER_SIZE:
+            screen.blit(player, player_rect)
+            screen.blit(
+                text.render(f'Штрафных очков: {round(penalty, 1)}', True, RED, None), text_xу)
+        else:
+            screen.blit(btn, ((WIN_WIDTH - BTN_W) // 2, WIN_HEIGHT // 2))
+            btn.blit(
+                text.render(text1, True, WHITE, None),
+                ((BTN_W - text1_xy[0]) // 2, (BTN_H - text1_xy[1]) // 2))
+            screen.blit(
+                text.render(f'Штрафных очков: {round(penalty, 1)}', True, RED, None),
+                ((WIN_WIDTH - text.size(f'Штрафных очков: {round(penalty, 1)}')[0]) // 2,
+                 WIN_HEIGHT // 2 - BTN_H))
 
-    pygame.display.set_caption(f' FPS: {round(clock.get_fps(), 2)}')
-    pygame.display.update()
-    clock.tick(FPS)
+        pygame.display.set_caption(f' FPS: {round(clock.get_fps(), 2)}')
+        pygame.display.update()
+        clock.tick(FPS)
